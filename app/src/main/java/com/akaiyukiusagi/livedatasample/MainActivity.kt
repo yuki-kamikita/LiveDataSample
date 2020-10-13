691c16ad0cf2ad8edbfa3c44bb35ec6721a6161c
shuhei-ff <s.kiyosawa@frontierfield.co.jp>
2020-10-13 18:05:37 +0900

    おまけ（１秒毎に自動で実行）追加

--- app/src/main/java/com/akaiyukiusagi/livedatasample/MainActivity.kt
+++ app/src/main/java/com/akaiyukiusagi/livedatasample/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.akaiyukiusagi.livedatasample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,11 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // LiveDataを初期化（？）
-        val sampleLiveData: MutableLiveData<Int> by lazy {
-            MutableLiveData<Int>()
+        // LiveDataを初期化
+        val sampleLiveData: MutableLiveData<LocalDateTime> by lazy {
+            MutableLiveData<LocalDateTime>()
         }
-        sampleLiveData.value = 0 // 初期値
 
         // Observerを定義
         // sampleLiveData の値が変化したら text_view.text を変更
@@ -24,10 +24,20 @@ class MainActivity : AppCompatActivity() {
             value?.let { text_view.text = it.toString() }
         })
 
-        // 背景タップで値+1
+        // 背景タップでLiveDataの値を変更
         constraint.setOnClickListener {
-            sampleLiveData.value = sampleLiveData.value!! + 1
+            sampleLiveData.value = LocalDateTime.now()
         }
 
+        // おまけ
+        // 1秒（1000ms）delay（待機）して実行
+//        val handler = Handler()
+//        val r: Runnable? = object : Runnable {
+//            override fun run() {
+//                sampleLiveData.value = LocalDateTime.now() // LiveDataに保存
+//                handler.postDelayed(this, 1000) // 1000ms待つ
+//            }
+//        }
+//        handler.post(r)
     }
 }
